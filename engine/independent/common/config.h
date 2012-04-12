e3bb0ee64ffae2c364ecb62b8f6d6260c6050e36
andyc <andyc@crytek.com>
2012-04-12 15:42:38 +0100

    default 60fps

--- engine/independent/common/config.h
+++ engine/independent/common/config.h
@@ -11,7 +11,7 @@
 #define DEFAULT_WINDOW_WIDTH (640)
 #define DEFAULT_WINDOW_HEIGHT (400)
 #define DEFAULT_WINDOW_TITLE "Framework Test"
-#define DEFAULT_FRAMERATE (10.0)
+#define DEFAULT_FRAMERATE (60.0)
 
 //==============================================================================
 #endif // End [!defined(__CONFIG_H__)]
